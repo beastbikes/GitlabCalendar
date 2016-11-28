26424e5ebac17521cd73847f8e53aa3130f7bdd7
ZhangBohan <bohanzhang@foxmail.com>
2016-11-28 12:05:34 +0800

    gitlab auth

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.5
-
 ENV PYTHONUNBUFFERED 1
+
 RUN mkdir /code
 WORKDIR /code
 ADD requirements.txt /code/
-RUN pip install -r requirements.txt
+RUN pip install -r requirements.txt -i http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 ADD app.py /code/
--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask, request
+import os
+import logging
+from datetime import datetime, timedelta
+
+import requests
+from flask import Flask, request, session
 app = Flask(__name__)
+
+GITLAB_HOST = os.environ['GITLAB_HOST']
+GITLAB_APPID = os.environ['GITLAB_APPID']
+GITLAB_APP_SECRET = os.environ['GITLAB_APP_SECRET']
 
 
 @app.route('/')
 def hello_world():
+    logging.debug('hello world')
     return 'Hello, World!'
 
 
 @app.route('/auth')
 def auth():
-    print('data', request.data)
+    code = request.args.get('code')
+
+    url = GITLAB_HOST + '/oauth/token'
+    params = {
+        "client_id": GITLAB_APPID,
+        "client_secret": GITLAB_APP_SECRET,
+        "code": code,
+        "grant_type": "AUTHORIZATION_CODE",
+        "redirect_uri": ""
+    }
+
+    r = requests.get(url, params)
+    data = r.json()
+    logging.debug('result:', data)
+    session['access_token'] = data['access_token']
+    session['expires_at'] = datetime.now() + timedelta(seconds=data['expires_in'])
+    session['refresh_token'] = data['refresh_token']
+
+    return "ok"
+
+
+@app.route("/token")
+def token():
     return "ok"
